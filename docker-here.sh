b0d59c4dc51daf32ee28cc160701251b41c3b641
you <you>
2019-04-24 19:49:30 +0800

    qdocker

--- docker-here.sh
+++ docker-here.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo umount -f /mnt/here
 sudo mkdir /mnt/here
-sudo mount -v -t cifs -o username="$1",password="$2",dir_mode=0777,file_mode=0777 //10.0.2.2/docker-here /mnt/here
+sudo mount -v -t cifs -o username="$2",password="$3",dir_mode=0777,file_mode=0777 //10.0.2.2/docker-here /mnt/here
+sudo rm /usr/local/bin/here
+sudo ln -s /root/user-here/here-run.sh /usr/local/bin/here
--- runsh.cmd
+++ runsh.cmd
@@ -8,6 +8,7 @@ call ssh "sudo chmod 777  ~/.runsh" > nul
 call scp_send %A% %F% > nul
 echo scp_send %A% %F%
 call ssh "sudo chmod 777 %F%" > nul
+shift
 call ssh %F% %* > nul
 endlocal
 popd
